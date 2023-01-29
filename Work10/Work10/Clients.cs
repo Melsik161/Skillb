ad1a2f439e4a8c90145ae951f07ec04cf4bdb338
Melsik161 <melsrnd@yandex.ru>
2023-01-30 01:17:28 +0300

    1

---
+++ Work10/Work10/Clients.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Work10
+{
+    class Clients
+    {
+        int id { get; set; }
+        string Surname { get; set; }
+        string Name { get; set; }
+        string middleName { get; set; }
+        string Passport { get; set; }
+
+        public Clients (int id, string Surname, string Name, string middleName, string Passport)
+        {
+            this.id = id;
+            this.Surname = Surname;
+            this.Name = Name;
+            this.middleName = middleName;
+            this.Passport = Passport;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{this.Surname}");
+        }
+    }
+   
+}
---
+++ Work10/Work10/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace Work10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Clients> db = new ObservableCollection<Clients>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int id = 0;
+            string line;
+            string[] tempLin;
+            using (StreamReader sr = new StreamReader("Clients.txt"))
+            { 
+
+                while ((line = sr.ReadLine()) != null)
+                {
+
+                    tempLin = line.Split('#');
+                    if (line == "") continue; // Если есть пустая строка в файле, то + итерация.
+                    db.Add(new Clients(++id, tempLin[1], tempLin[2], tempLin[3], tempLin[4]));
+
+                }
+                
+            }
+            
+            ClientList.ItemsSource = db;
+        }
+    }
+}
