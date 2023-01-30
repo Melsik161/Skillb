de79d6a8da34350177763eb7e1385559160f7d56
Melsik161 <melsrnd@yandex.ru>
2023-01-30 12:35:07 +0300

    2

--- Work10/Work10/Clients.cs
+++ Work10/Work10/Clients.cs
@@ -10,23 +10,20 @@ namespace Work10
     class Clients
     {
         int id { get; set; }
-        string Surname { get; set; }
-        string Name { get; set; }
-        string middleName { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string middleName { get; set; }
+        public string phoneNumber { get; set; }
         string Passport { get; set; }
 
-        public Clients (int id, string Surname, string Name, string middleName, string Passport)
+        public Clients (int id, string Surname, string Name, string middleName, string phoneNumber, string Passport)
         {
             this.id = id;
             this.Surname = Surname;
             this.Name = Name;
             this.middleName = middleName;
+            this.phoneNumber = phoneNumber;
             this.Passport = Passport;
-        }
-
-        public override string ToString()
-        {
-            return String.Format($"{this.Surname}");
         }
     }
    
--- Work10/Work10/MainWindow.xaml.cs
+++ Work10/Work10/MainWindow.xaml.cs
@@ -22,33 +22,53 @@ namespace Work10
     /// </summary>
     public partial class MainWindow : Window
     {
+
         ObservableCollection<Clients> db = new ObservableCollection<Clients>();
         public MainWindow()
         {
             InitializeComponent();
-            
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
             int id = 0;
             string line;
             string[] tempLin;
             using (StreamReader sr = new StreamReader("Clients.txt"))
-            { 
+            {
 
                 while ((line = sr.ReadLine()) != null)
                 {
 
                     tempLin = line.Split('#');
                     if (line == "") continue; // Если есть пустая строка в файле, то + итерация.
-                    db.Add(new Clients(++id, tempLin[1], tempLin[2], tempLin[3], tempLin[4]));
+                    db.Add(new Clients(++id, tempLin[1], tempLin[2], tempLin[3], tempLin[4], tempLin[5]));
 
                 }
-                
+
             }
-            
             ClientList.ItemsSource = db;
+
+        }
+
+        private void ClientList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lSurname.Content = db[ClientList.SelectedIndex].Surname;
+            lName.Content = db[ClientList.SelectedIndex].Name;
+            lMiddleName.Content = db[ClientList.SelectedIndex].middleName;
+            lNumber.Content = db[ClientList.SelectedIndex].phoneNumber;
+
+            if (WorkerCons.IsChecked == true) lPassport.Content = "***********";
+
+        }
+
+        private void SaveEdit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Номер изменен!\nБыл {lNumber.Content}\nСтал {EditInform.Text}");
+            lNumber.Content = EditInform.Text;
+            db[ClientList.SelectedIndex].phoneNumber = EditInform.Text;
+            EditInform.Clear();
+        }
+
+        private void lNumber_MouseDoubleClick_1(object sender, MouseButtonEventArgs e)
+        {
+            EditInform.Text = lNumber.Content.ToString();
         }
     }
 }
